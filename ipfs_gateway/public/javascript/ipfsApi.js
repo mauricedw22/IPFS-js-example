19298f9ae43ad1d6a111d96ef0c51201454fc9bc
Maurice WALKER <mauricedw22@gmail.com>
2021-10-19 00:10:20 -0400

    added ipfs api 2

---
+++ ipfs_gateway/public/javascript/ipfsApi.js
@@ -0,0 +1,37 @@
+const IPFS = require('ipfs');
+const all = require('it-all');
+
+let node;
+async function init() {
+  node = await IPFS.create();
+  console.log('IPFS node initialised.');
+}
+
+const ipfsApi = {
+  async ipfsInit() {
+    try {
+      const node = await IPFS.create();
+      return node;
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  async ipfsAdd(data) {
+    try {
+      return await node.add(data);
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  async ipfsGet(cid) {
+    try {
+      const data = Buffer.concat(await all(node.cat(cid)));
+      return { 0: data.toString() };
+    } catch (e) {
+      console.log(e);
+    }
+  },
+};
+init();
+
+module.exports = ipfsApi;
